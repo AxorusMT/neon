0d0a4b80cdf8fdc4c699f6276c1f5515cf28fb3a
Mwiningban Ibrahim <ftmwini@gmail.com>
2022-08-08 02:38:22 +0100

    Add update, draw, add and remove functions to SceneManager struct

--- src/scene.rs
+++ src/scene.rs
@@ -1,4 +1,5 @@
 use std::rc::Rc;
+use std::collections::HashMap;
 use crate::application::Application;
 
 
@@ -9,16 +10,47 @@ pub trait Scene {
     fn on_leave(&self) -> ();
     fn process_input(&self) -> ();
     fn update(&self) -> ();
-    fn draw(&self) -> ();
+    fn draw(&self, app: Application) -> ();
 }
 
 pub struct SceneManager {
-    scenes: Vec<Rc<dyn Scene>>,
-    current_scene: Option<Rc<dyn Scene>>
+    scenes: HashMap<u32, Rc<dyn scene>>,
+    current_scene: Rc<dyn Scene>,
+    id: u32
 }
 
 impl SceneManager {
-    fn process_input(&self) -> () {
-
+    pub fn process_input(&self) -> () {
+        self.current_scene.process_input();
     }
+
+    pub fn update(&self) -> () {
+        self.current_scene.update();
+    }
+
+    pub fn draw(&self, app: Application) -> () {
+        self.current_scene.draw(app);
+    }
+
+    pub fn add_scene(&mut self, scene: Rc<dyn Scene>) -> u32 {
+        self.scenes.insert(self.id, scene);
+        scene.on_create();
+        self.id = self.id + 1;
+        self.id - 1
+    }
+
+    // I apologise for how shit this code is. It is temporary.
+    pub fn remove(&mut self, id: u32) -> () {
+        for (k, v) in self.scenes.iter() {
+            if k == id {
+                if let Some(scene) = self.scenes.get(&k) {
+                    let scene = scene as Rc<dyn Scene>;
+                    scene.on_destroy();
+                }
+            }
+        }
+        self.scenes.retain(|k, _| k != id);
+    }
+
+
 }
